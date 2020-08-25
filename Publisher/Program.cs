f24f0cb1f0388b206c02e7a6e6c908b483ced686
심승용 <seungyong.shim@mirero.co.kr>
2020-08-25 17:14:19 +0900

    최초

---
+++ Publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = ConnectionMultiplexer.Connect("localhost");
+            var pubsub = client.GetSubscriber();
+
+            pubsub.Publish("id1", "HelloWorld");
+            
+        }
+    }
+}
